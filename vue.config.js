fec79c68c96ba52561162f49f1476aeab2d22558
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2020-12-09 20:04:26 +0000

    added browser extension

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  pages: {
+    popup: {
+      template: 'public/browser-extension.html',
+      entry: './src/popup/main.js',
+      title: 'Popup'
+    },
+    override: {
+      template: 'public/browser-extension.html',
+      entry: './src/override/main.js',
+      title: 'Override'
+    }
+  },
+  pluginOptions: {
+    browserExtension: {
+      componentOptions: {
+        background: {
+          entry: 'src/background.js'
+        },
+        contentScripts: {
+          entries: {
+            'content-script': [
+              'src/content-scripts/content-script.js'
+            ]
+          }
+        }
+      }
+    }
+  }
+}
