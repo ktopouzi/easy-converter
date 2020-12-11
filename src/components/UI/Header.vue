c1cc0c749b7d274d8d10edf9c87eb935f90ac4e7
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2020-12-11 17:53:07 +0000

    created  header component

---
+++ src/components/UI/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <nav class="flex justify-between">
+      <router-link to="/">TEST</router-link>
+      <router-link to="/about">TEST</router-link>
+      <router-link to="/about">TEST</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
