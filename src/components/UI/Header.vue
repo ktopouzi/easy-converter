43ad31efb597e4784846ee9024b4be7b36782b1b
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2020-12-18 23:02:12 +0000

    updated header template

--- src/components/UI/Header.vue
+++ src/components/UI/Header.vue
@@ -1,9 +1,9 @@
 <template>
-  <header>
+  <header class="bg-cvrt_primary text-white text-base">
     <nav class="flex justify-between">
-      <router-link to="/">TEST</router-link>
-      <router-link to="/about">TEST</router-link>
-      <router-link to="/about">TEST</router-link>
+      <router-link class="p-4" to="/">Convert</router-link>
+      <router-link class="p-4" to="/rates">Rates</router-link>
+      <router-link class="p-4" to="/settings">Settings</router-link>
     </nav>
   </header>
 </template>
